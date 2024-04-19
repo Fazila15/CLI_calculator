ef4744ba22186f933809c336864f6bf332e9ff38
fazila15e <fazilamalik15@gmail.com>
2024-04-20 03:54:10 +0500

    Chalk Added.

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from 'inquirer';
-const answer = await inquirer.prompt([{ message: "Enter First Number", type: "number", name: "firstNumber" },
-    { message: "Enter Second Number", type: "number", name: "secondNumber" },
-    { message: "choose operator",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+let condition = true;
+while (condition) {
+    const answer = await inquirer.prompt([
+        {
+            name: "firstNumber",
+            message: `${chalk.cyanBright("Enter First Number: ")}`,
+            type: "number",
+        },
+        {
+            name: "secondNumber",
+            message: `${chalk.cyanBright("Enter Second Number: ")}`,
+            type: "number",
+        },
+        {
+            name: "operator",
+            message: `${chalk.cyan("Operation: ")}`,
+            type: "list",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        }
+    ]);
+    if (answer.operator === "Addition") {
+        console.log(answer.firstNumber + answer.secondNumber);
     }
-]);
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+    else if (answer.operator === "Subtraction") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operator === "Multiplication") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operator === "Division") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    ;
+    let usrchoice = await inquirer.prompt({
+        name: "option",
+        type: "list",
+        message: `${chalk.yellow.bold("Select an option: ")}`,
+        choices: ["Calculate Again", "Exit"]
+    });
+    if (usrchoice.option === "Exit") {
+        condition = false;
+    }
+    ;
 }
-else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
-}
+;
--- index.ts
+++ index.ts
@@ -1,21 +1,54 @@
 #! /usr/bin/env node
-import inquirer from 'inquirer'
-const answer = await inquirer.prompt([{message: "Enter First Number", type: "number", name: "firstNumber"},
 
-{message: "Enter Second Number", type: "number", name: "secondNumber"},
+import chalk from "chalk";
+import inquirer from 'inquirer';
+let condition = true;
 
-{message: "choose operator",
-type: "list",
-name: "operator",
-choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-}
-]);
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber)
-    } else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber)
-    } else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber)
-    } else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber)
+while (condition)
+{const answer = await inquirer.prompt(
+    [
+        {
+            name: "firstNumber",
+            message: `${chalk.cyanBright("Enter First Number: ")}`, 
+            type: "number",
+            
+        },
+        {
+            name: "secondNumber",
+            message: `${chalk.cyanBright("Enter Second Number: ")}`, 
+            type: "number", 
+        },
+        {
+            name: "operator",
+            message: `${chalk.cyan("Operation: ")}`,
+            type: "list",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        }
+   ]
+);
+if (answer.operator === "Addition") 
+    {
+      console.log (answer.firstNumber + answer.secondNumber)
+    } else if (answer.operator === "Subtraction") 
+    {
+      console.log (answer.firstNumber - answer.secondNumber)
+    } else if (answer.operator === "Multiplication")
+    {
+      console.log (answer.firstNumber * answer.secondNumber)
+    } else if (answer.operator === "Division") 
+    {
+      console.log (answer.firstNumber / answer.secondNumber)
+    };
+let usrchoice = await inquirer.prompt(
+    {
+        name: "option",
+        type: "list",
+        message: `${chalk.yellow.bold("Select an option: ")}`,
+        choices: ["Calculate Again","Exit"]
     }
+); 
+if (usrchoice.option === "Exit")
+    {
+       condition = false
+    };
+};
